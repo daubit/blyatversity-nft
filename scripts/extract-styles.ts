d7717f511997091779d9df69563ee660ca27a620
Sono Willbrand <sono.willbrand@daubit.org>
2023-01-24 14:26:29 +0100

    extract styles

--- scripts/extract-styles.ts
+++ scripts/extract-styles.ts
@@ -2,7 +2,7 @@ import { readdirSync, readFileSync, writeFileSync } from "fs";
 import parse from "node-html-parser";
 
 const ROOT_FOLDER = "assets";
-const attributes = readdirSync(ROOT_FOLDER).filter(attribute => attribute !== "styles");
+const attributes = readdirSync(ROOT_FOLDER).filter(attribute => attribute !== "_styles");
 let styles = "";
 for (const attribute of attributes) {
     const files = readdirSync(`${ROOT_FOLDER}/${attribute}`)
@@ -13,9 +13,10 @@ for (const attribute of attributes) {
         const defs = group.childNodes.find((node: any) => node.rawTagName === "defs")!
         if (!defs) continue;
         styles += defs.childNodes.map(node => node.toString()).join("");
+        console.log(group.childNodes)
         group.childNodes = group.childNodes.filter((node: any) => node.rawTagName !== "defs")
-        html.exchangeChild(html.firstChild, group);
-        writeFileSync(`${ROOT_FOLDER}/${attribute}/${fileName.replace(".svg", ".html")}`, file);
+        html = html.exchangeChild(html.firstChild, group);
+        writeFileSync(`${ROOT_FOLDER}/${attribute}/${fileName.replace(".svg", ".html")}`, html.toString());
     }
 }
 writeFileSync(`${ROOT_FOLDER}/_styles/default.html`, styles);
