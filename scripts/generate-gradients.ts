a414fdaed45d6b605203a6b9863a7235f90d5029
Sono Willbrand <sono.willbrand@daubit.org>
2023-01-25 17:40:08 +0100

    fix gradient

--- scripts/generate-gradients.ts
+++ scripts/generate-gradients.ts
@@ -3,18 +3,16 @@ import randomColor from "randomcolor"
 import { writeFileSync } from "fs"
 
 const gradientColors = gradients.map((gradient => gradient.match(/(#)([a-f]|[0-9]){6}/g)))
-
 const template = (gradients: string) => `<defs>\n${gradients}\n</defs>`
 // const colors = randomColor({ count: classes.length })
 // const styles = classes.map((className, i) => `${className} {fill: ${colors[i]};}`).join("\n")
 const genGradients = gradients.map((gradient, i) => {
     const random = randomColor();
     const newColors = randomColor({ hue: random, count: gradientColors[i]?.length ?? 0 })
-    let result = ""
+    let result = gradient
     for (let j = 0; j < gradientColors[i]?.length!; j++) {
         const oldColor = gradientColors[i]![j];
-        result = gradient.replace(oldColor, newColors[j])
-        console.log(result)
+        result = result.replace(oldColor, newColors[j])
     }
     return result;
 }).join("")
