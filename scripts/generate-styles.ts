717931a84549a7314387839bc6108aac3fab85e4
Sono Willbrand <sono.willbrand@daubit.org>
2023-01-26 16:17:29 +0100

    refactor

--- scripts/generate-styles.ts
+++
@@ -1,8 +0,0 @@
-import classes from "../data/classes.json"
-import randomColor from "randomcolor"
-import { writeFileSync } from "fs"
-
-const template = (innerText: string, gradients: string) => `<defs><style>${innerText}</style>${gradients}</defs>`
-const colors = randomColor({ count: classes.length })
-const styles = classes.map((className, i) => `${className} {fill: ${colors[i]};}`).join("\n")
-writeFileSync("assets/Layer_4/_Styles/var_1.html", template(styles, ""))
