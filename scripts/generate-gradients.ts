bee0d32d972fdb1fb868f59ad8c88b907c6a959c
Sono Willbrand <sono.willbrand@daubit.org>
2023-01-26 10:36:22 +0100

    generate gradients

--- scripts/generate-gradients.ts
+++ scripts/generate-gradients.ts
@@ -2,19 +2,22 @@ import gradients from "../data/gradients.json"
 import randomColor from "randomcolor"
 import { writeFileSync } from "fs"
 
-const gradientColors = gradients.map((gradient => gradient.match(/(#)([a-f]|[0-9]){6}/g)))
-const template = (gradients: string) => `<defs>\n${gradients}\n</defs>`
-// const colors = randomColor({ count: classes.length })
-// const styles = classes.map((className, i) => `${className} {fill: ${colors[i]};}`).join("\n")
-const genGradients = gradients.map((gradient, i) => {
-    const random = randomColor();
-    const newColors = randomColor({ hue: random, count: gradientColors[i]?.length ?? 0 })
-    let result = gradient
-    for (let j = 0; j < gradientColors[i]?.length!; j++) {
-        const oldColor = gradientColors[i]![j];
-        result = result.replace(oldColor, newColors[j])
-    }
-    return result;
-}).join("")
+const amount = 10;
+for (let k = 0; k < amount; k++) {
+    const gradientColors = gradients.map((gradient => gradient.match(/(#)([a-f]|[0-9]){6}/g)))
+    const template = (gradients: string) => `<defs>\n${gradients}\n</defs>`
+    // const colors = randomColor({ count: classes.length })
+    // const styles = classes.map((className, i) => `${className} {fill: ${colors[i]};}`).join("\n")
+    const genGradients = gradients.map((gradient, i) => {
+        const random = randomColor();
+        const newColors = randomColor({ hue: random, count: gradientColors[i]?.length ?? 0 })
+        let result = gradient
+        for (let j = 0; j < gradientColors[i]?.length!; j++) {
+            const oldColor = gradientColors[i]![j];
+            result = result.replace(oldColor, newColors[j])
+        }
+        return result;
+    }).join("")
 
-writeFileSync("assets/Layer_4/_Styles/grad_1.html", template(genGradients))
+    writeFileSync(`assets/Layer_4/_Styles/grad_${k}.html`, template(genGradients))
+}
