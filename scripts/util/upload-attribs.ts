217a9eddddadf54444e0b73c52b0367d1aa716da
Alexander Krahl <alexander.krahl@daubit.org>
2023-01-30 10:54:25 +0100

    fix padding breaking scripts

--- scripts/util/upload-attribs.ts
+++ scripts/util/upload-attribs.ts
@@ -4,6 +4,7 @@ import { minify } from "html-minifier";
 import { encode } from "js-base64";
 // @ts-ignore
 import { MetadataFactory } from "../../typechain-types";
+import { pad, PadType } from "./padding";
 
 export interface Variant {
 	name: string;
@@ -123,15 +124,15 @@ export async function uploadVariants(metadata: MetadataFactory, ROOT_FOLDER: Pat
 					caseSensitive: true,
 				}),
 			}));
+			const padType = attribute.includes("_Scripts") ? PadType.Script : PadType.Svg;
+
 			for (const variant of variants) {
 				const { svg, name } = variant;
 				const chunkSize = 5_000;
 				for (let start = 0; start < svg.length; start += chunkSize) {
 					const till = start + chunkSize < svg.length ? start + chunkSize : svg.length;
-					let svgChunk = svg.slice(start, till);
-					while (encode(svgChunk, false).endsWith("=")) {
-						svgChunk += " ";
-					}
+					const svgChunk = pad(svg.slice(start, till), padType);
+
 					const addVariantChunkedTx = await metadata.addVariantChunked(
 						attributeId,
 						name,
